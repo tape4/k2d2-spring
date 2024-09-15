9e4b2f58fbc9bbd32f7115ebf503a1557296eac1
JeongHoon Lee <sherry0_@kakao.com>
2024-09-15 19:49:09 +0900

    cors

---
+++ src/main/java/knet_chanllenge/k2d2/K2d2Configuration.java
@@ -0,0 +1,18 @@
+package knet_chanllenge.k2d2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class K2d2Configuration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
